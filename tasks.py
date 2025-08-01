dfcf1a4c6c2ed1646c9b6db88a84314e3d274d69
Shreyas <shreyas@iamdave.ai>
2025-08-01 19:34:42 +0530

    tasks templates

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 from gryd_worker import gryd
-from utils import GRYD_SERVICE, GRYD_CONFIG
+from utils import GRYD_SERVICE, GRYD_CONFIG, get_logger
+
+logger = get_logger(__name__)
 
 gryd.SERVICE = GRYD_SERVICE
 gryd.set_queue_manager(config = GRYD_CONFIG)
@@ -38,13 +40,20 @@ def autobot_agents_trigger(*args, **kwargs):
         prioritization_agent_results = gryd.await_results(prioritization_agent)
         return propensity_agent_results, competitor_analysis_agent_results, prioritization_agent_results
     
-    
+@gryd.is_a_task(function_name="propensity_agent")    
 def propensity_agent(*args, **kwargs):
     return "propensity_agent_results"
 
+@gryd.is_a_task(function_name="competitor_analysis_agent")
 def competitor_analysis_agent(*args, **kwargs):
     return "competitor_analysis_agent_results"
 
+@gryd.is_a_task(function_name="prioritization_agent")
 def prioritization_agent(*args, **kwargs):
     return "prioritization_agent_results"
+
+
+def results(*args, **kwargs):
+    return "results"
+
 
