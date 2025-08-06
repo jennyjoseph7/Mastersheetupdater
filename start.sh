662829657e2a992daf78fc9afc4e841939d8b28e
Shreyas <shreyas@iamdave.ai>
2025-08-06 13:56:19 +0530

    removed credentials added by rishikesh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export ENVIRONMENT=local
-export AUTH_HOST_USER_ID=shreyas+file-gryd@iamdave.ai
-export AUTH_HOST=https://staging.iamdave.ai
-export AUTH_HOST_API=917d51b5-7f85-3a21-bdec-548fa0bdbe70
-export AUTH_HOST_ENTERPRISE_ID=gryd_file_system
+export AUTH_HOST_USER_ID=<YOUR_USER_ID>
+export AUTH_HOST=<YOUR_AUTH_HOST>
+export AUTH_HOST_API=<YOUR_API_KEY>
+export AUTH_HOST_ENTERPRISE_ID=<YOUR_ENTERPRISE_ID>
 worker -m tasks.py -n 8
