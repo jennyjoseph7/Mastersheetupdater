baf8ef9780c3fda42f15d25cdb5ed9b733858241
Jay Telgote <jay.telgote@iamdave.ai>
2025-08-31 12:18:06 +0530

    competitor agent has been fixed

--- agents/competitor_analysis_agent/get_models.py
+++ agents/competitor_analysis_agent/get_models.py
@@ -5,7 +5,7 @@ enp_id=os.environ.get("CARDB_ENTERPRISE_ID")
 user_id=os.environ.get("CARDB_USER_ID")
 api_key=os.environ.get("CARDB_API_KEY")
 
-def car_models(top_n,model=None,exclude=None):
+def car_models(top_n=5,model=None,exclude=None):
     """
     Get a list of car models
     Parameters
@@ -19,9 +19,9 @@ def car_models(top_n,model=None,exclude=None):
         json string containing the list of car models
     """
     if model and exclude:
-        url = f"https://test.iamdave.ai/objects/model_variant_analysis?model={model}&brand~={exclude}"
+        url = f"https://test.iamdave.ai/objects/model_variant_analysis?product_name={model}&brand_name~={exclude}"
     elif model:
-        url = f"https://test.iamdave.ai/objects/model_variant_analysis?model={model}"
+        url = f"https://test.iamdave.ai/objects/model_variant_analysis?product_name={model}"
     else:
         url = "https://test.iamdave.ai/objects/model_variant_analysis"
 
@@ -37,4 +37,4 @@ def car_models(top_n,model=None,exclude=None):
     return json.loads(response.text)['data'][:top_n]
 
 if __name__ == "__main__":
-    print(car_models("DB11"))
+    print(car_models(5,"Fronx"))
