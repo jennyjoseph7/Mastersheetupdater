11c36bef84d3b0939402f1651cca2adc06bc936f
soham dutta <soham@iamdave.ai>
2025-11-08 23:13:57 +0530

    added dummy functions

--- autocrm_db_helper/PGConnector.py
+++ autocrm_db_helper/PGConnector.py
@@ -8,4 +8,5 @@ class AutoCRMPGConnector(db.GrydPGConnector):
     def __init__(self, enterprise_id, *args, **kwargs):
         super().__init__(enterprise_id, *args, **kwargs)
     
-    
+    def update(self, table_name, id_attr, id, data, additional = "", additional_values = None):
+        super().update(table_name, id, data, id_attr, additional, additional_values) 
