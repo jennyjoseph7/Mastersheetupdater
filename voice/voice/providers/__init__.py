ec9927f65c63fb0a62bd5bad8523e7a428851576
Nikit P <nikit@iamdave.ai>
2025-11-25 12:07:24 +0530

    update voice package

--- voice/voice/providers/__init__.py
+++ voice/voice/providers/__init__.py
@@ -1,5 +1,4 @@
 """Providers package."""
 from .provider_base import ProviderBase
-from .twilio_provider import TwilioProvider
 
-__all__ = ["ProviderBase", "TwilioProvider"]
+__all__ = ["ProviderBase"]
