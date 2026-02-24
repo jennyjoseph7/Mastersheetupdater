d49e26cecda9ee5b6677e09aec36b4a243bbc40b
Praveen <praveen@iamdave.ai>
2026-02-24 13:01:12 +0530

    added resolve_and_search_in_model func

--- autocrm_validator.py
+++ autocrm_validator.py
@@ -30,6 +30,26 @@ def get_transaction_id(ins, *args, **kwargs):
 def get_campaign_end_date_func(ins, campaign_id, campaign_end_date, **kwargs):
     pass
 
+def resolve_and_search_in_model_func(ins,**kwargs):
+
+    model_name = kwargs.pop('model_name', None)
+    attribute_name = kwargs.pop('attribute_name', None)
+    lead_id = kwargs.pop('lead_id', None)
+    _get_lead_id = kwargs.pop('_get_lead_id', False)
+    if not model_name:
+        raise ValueError("model_name is required for search_in_model")
+
+    if model_name and lead_id and _get_lead_id:
+        lead_model_id=f"{model_name}_id"
+        kwargs[lead_model_id]=lead_id
+        
+    return val.search_among_model_func(
+        ins,
+        model_name,
+        attribute_name,
+        **kwargs
+    )
+ 
 val.make_function(
     calulate_total_billing_func,
     "calulate_total_billing_amount",
@@ -38,6 +58,13 @@ val.make_function(
     help_string = "Function to calculate billing total amount for dealership."
 )
 
+val.make_function(
+    resolve_and_search_in_model_func,
+    "resolve_and_search_in_model",
+    given_args="instance",
+    is_idempotent=False, 
+    help_string = "Resolve model and attribute dynamically, then search and return the first matching result or attribute value."
+)
 val.make_function(
     generate_billing_invoice_func,
     "generate_billing_invoice",
