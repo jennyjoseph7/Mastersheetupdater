2878a8aa64673ae5862f8061203e8c10f015afcc
Praveen <praveen@iamdave.ai>
2026-03-03 15:23:54 +0530

    updating transcript_summary

--- voice/voice/providers/elevanlabs_tatatele.py
+++ voice/voice/providers/elevanlabs_tatatele.py
@@ -977,8 +977,9 @@ def process():
     xx = gryd_tasks.post_billing_object("completed", session_id, duration)  # call it in async
 
     logger.info(f"Billing record created: {xx}")
-    logger.info(f"Received data from tatatele ---> {json.dumps(data, indent=4)} and summary --> {data.get('summary')}")
     session_history = format_transcript(data.get("transcript", []), data.get("metadata", {}).get("start_time_unix_secs", time()))
+    transcript_summary= data.get("analysis",{}).get("transcript_summary")
+    logger.info(f"Transcript summary: {transcript_summary}")
     logger.info(f"Triggering post history and actions for session_id: {session_id}")
 
     gryd_tasks.post_history(session_id, session_history)
@@ -987,7 +988,8 @@ def process():
         "session_id": session_id,
         "additional_dict":{
             "history": session_history,
-            "status": "completed"
+            "status": "completed",
+            "summary": transcript_summary
         }
     })
     
