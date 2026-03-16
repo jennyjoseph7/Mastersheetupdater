652b1300ded79cc76459bcbceef08cd17d23440f
Praveen <praveen@iamdave.ai>
2026-03-16 17:54:35 +0530

    added template_message and setup my code to call ananth's next campaign worflow

--- communication/connectors/whatsapp_connectors/source_connectors.py
+++ communication/connectors/whatsapp_connectors/source_connectors.py
@@ -638,8 +638,9 @@ class BaseWebhookConverter:
 
         kwargs["temporary_data"] = temporary_data
         logger.info("Calling session logic...")
+        
         # call session logic here...
-        d=handle_session_logic(mobile_number,"whatsapp_chat")
+        d=handle_session_logic(mobile_number,"whatsapp_chat",True)
         logger.info(f"Session logic result: {d}")
         user_d=temporary_data.get("whatsapp_user_details")
         converse_kwargs.update({
