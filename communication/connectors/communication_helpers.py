606d780207129079b3151bfdfc6ea51c342b7517
Praveen <praveen@iamdave.ai>
2026-03-23 17:25:54 +0530

    calling determine_campaign_next_action

--- communication/connectors/communication_helpers.py
+++ communication/connectors/communication_helpers.py
@@ -273,13 +273,13 @@ def get_or_create_session(data,channel=None,engaged=False):
                 # Case 2: converted 
                 elif previous_disposition == "converted":
                     logger.info(f"Session {session_id} already converted. Handling converted session logic.")
-                    logger.info(f"Calling ananth's task for disposition {previous_disposition} and the session_id: {session_id}")
-                    # call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,data.get("disposition"),pg=pg)
+                    logger.info(f"Calling determine_campaign_next_action for disposition {previous_disposition} and the session_id: {session_id}")
+                    call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,data.get("disposition"),pg=pg)
                 # Case 3: anything else → update the disposition to engaged
                 else:
                     logger.info(f"Since the user has interacted . Updating the disposition from {previous_disposition} to engaged for session {session_id}.")
-                    logger.info(f"Calling ananth's task and the session_id: {session_id}--> diposition is set to engaged.")
-                    # call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,"engaged",pg=pg)
+                    logger.info(f"Calling determine_campaign_next_action for the session_id: {session_id}--> diposition is set to engaged.")
+                    call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,"engaged",pg=pg)
 
                     pg.update("session","session_id",session_id,{"disposition":"engaged"})
                     
