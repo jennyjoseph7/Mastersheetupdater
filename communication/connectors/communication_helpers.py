411b1c4f216d94e97bfe7d0f4309eb8309a5139e
Praveen <praveen@iamdave.ai>
2026-03-26 12:43:14 +0530

    commented out calling next_campaign_workflow in master

--- communication/connectors/communication_helpers.py
+++ communication/connectors/communication_helpers.py
@@ -276,13 +276,13 @@ def get_or_create_session(data,channel=None,engaged=False):
                 elif previous_disposition == "converted":
                     logger.info(f"Session {session_id} already converted. Handling converted session logic.")
                     logger.info(f"Calling determine_campaign_next_action for disposition {previous_disposition} and the session_id: {session_id}")
-                    call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,data.get("disposition"),pg=pg)
+                    # call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,data.get("disposition"),pg=pg)
                 # Case 3: anything else → update the disposition to engaged
                 else:
                     if engaged:
                         logger.info(f"Since the user has interacted . Updating the disposition from {previous_disposition} to engaged for session {session_id}.")
                         logger.info(f"Calling determine_campaign_next_action for the session_id: {session_id}--> diposition is set to engaged.")
-                        call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,"engaged",pg=pg)
+                        # call_next_campaign_workflow_task(sessions[0].get("campaign_id"),sessions[0].get("campaign_type"),sessions[0].get("lead_id"),sessions[0].get("channel"),channel_identifier,"engaged",pg=pg)
 
                     pg.update("session","session_id",session_id,{"disposition":"engaged"})
                     
@@ -431,7 +431,7 @@ def create_new_session(data,channel=None,engaged=False):
         
         # updating lead last_session_channel 
         if data.get("campaign_type") == "pre-sales":
-            pg.update("pre_sales_lead","pre_sales_lead_id",s.get("lead_id"),{"last_session_channel":channel})
+            pg.update("pre_sales_lead","pre_sales_lead_id",s.get("lead_id"),{"last_session_channel":channel,"user_id":data.get("user_id")})
         elif data.get("campaign_type") == "post-sales":
             pg.update("post_sales_lead","post_sales_lead_id",s.get("lead_id"),{"last_session_channel":channel})
         return s 
