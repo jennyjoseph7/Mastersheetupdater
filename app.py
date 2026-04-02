84454109cb52a029d9ef49502dfaf616093ef553
Ananth Krishnan <ggananth@gmail.com>
2026-04-02 15:45:17 +0530

    Fixed issue with app.py for imports

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import hmac
+import hashlib
 from gryd_worker import gryd, gryd_routes, gryd_helpers as hp, gryd_db_helper as dbhp, beats as cron_worker
 from gryd_worker.gryd_routes import payload_decorator, signup_decorator
 from models import model as base_model
@@ -8,16 +11,13 @@ from db_routes import db_routes, ai_service_app
 from voice.voice.providers.twilio import app as twilio_routes
 from voice.voice.providers.elevanlabs_tatatele import app as elevanlabs_tatatele_routes
 from voice.voice.providers.elevanlab import app as elevanlab_routes
+from core.razorpay_service import razorpay_webhook_handler
+from core.core import generate_otp, dealership_signup, reset_password
 from cohorts_new.routes.routes import cohort_bp, gryd_orchestration_bp
 import os
 from flask import Flask,request,jsonify
 from config import *
 import autocrm_validator
-import json
-import hmac
-import hashlib
-from core.razorpay_service import razorpay_webhook_handler
-from core.core import generate_otp, dealership_signup, reset_password
 
 gryd.SERVICE = f"{AUTOCRM_APP_ENTERPRISE_ID}-app"   
 QM = gryd.set_queue_manager()
