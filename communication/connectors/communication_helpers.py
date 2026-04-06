555e89ef65afa6935c1866b5c85196b2082b57d1
Praveen <praveen@iamdave.ai>
2026-04-06 22:08:26 +0530

    resolved issues communication_helpers

--- communication/connectors/communication_helpers.py
+++ communication/connectors/communication_helpers.py
@@ -111,8 +111,9 @@ def handle_session_logic(phone_number, channel=None,engaged=False,campaign_detai
 
             campaign_model= "post_sales_campaign" if campaign_type == "post-sales" else "pre_sales_campaign"
             lead_model = "post_sales_lead" if campaign_type == "post-sales" else "pre_sales_lead"
+            lead_model_id="post_sales_lead_id" if campaign_type == "post-sales" else "pre_sales_lead_id"
             if lead_id and lead_model:
-                l=pg.get(lead_model,{f"{lead_model}_id"}, lead_id)
+                l=pg.get(lead_model,lead_model_id, lead_id)
                 if not l:
                     logger.info(f"No lead found for lead_id: {lead_id} in model: {lead_model}")
                 # l=l[0] if l else {}
