a0cab47b828fa6d50b3cb267dff4728427a99b5c
Praveen <praveen@iamdave.ai>
2026-04-15 18:32:21 +0530

    pushed the common_function file

---
+++ communication/common_functions.py
@@ -0,0 +1,49 @@
+
+import uuid
+import json
+from gryd_worker import gryd, gryd_helpers as hp,gryd_db_helper as db
+logger=gryd.logger
+from autocrm_db_helper import get_pg_connector
+
+def generate_uid(data):
+    if isinstance(data, (dict, list)):
+        data_str = json.dumps(data, sort_keys=True)
+    else:
+        data_str = str(data)
+
+    uid = uuid.uuid3(uuid.NAMESPACE_DNS, data_str)
+
+    return uid.hex[:16]
+
+
+def get_communication_credential(dealership_id="daveai", channel=None):
+    logger.info(f"Getting communication credential for dealership - {dealership_id}")
+
+    if not channel:
+        logger.info(
+            f"Channel not provided for dealership - {dealership_id}. Returning None."
+        )
+        return None
+
+    with get_pg_connector() as pg:
+        creds = list(pg.list("communication_credential",{"dealership_id": dealership_id, "channel": channel}))
+        if creds:
+            return creds[0]
+
+        # Fallback to default dealership "daveai" if no creds found for the dealership and channel
+        if dealership_id != "daveai":
+            logger.info(
+                f"No credential found for dealership - {dealership_id}. "
+                f"Falling back to default dealership - daveai for channel - {channel}"
+            )
+            creds = list(
+                pg.list(
+                    "communication_credential",
+                    {"dealership_id": "daveai", "channel": channel}
+                )
+            )
+            if creds:
+                return creds[0]
+
+    return None
+   
