ec891a5135b951523b1ca1824030307fd33da776
dave_gateway <dave@iamdave.ai>
2026-04-21 06:37:48 +0000

    updated the repo name in build_base_image.sh

--- docker/baseimg/build_base_image.sh
+++ docker/baseimg/build_base_image.sh
@@ -3,13 +3,13 @@
 function update_repo() {
 	sha=$1
 	git reset --hard HEAD^
-	git pull origin production
+	git pull origin $1
 	git checkout $1
 
 }
 
 function main() {
-	update_repo "production"
+	update_repo "master"
 	cp ../../requirements.txt ./
       	cp ../../spark/requirements.txt ./spark_requirements.txt
 	docker build -t autobot_prod_baseimage:latest .	
