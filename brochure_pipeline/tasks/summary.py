390502f16cccfa263ad403ab22049cb83157f6d2
ravi000111 <ravis541242@gmai.com>
2026-04-29 11:52:06 +0000

    updated summary.py

--- brochure_pipeline/tasks/summary.py
+++ brochure_pipeline/tasks/summary.py
@@ -1,6 +1,11 @@
 import os
 import json
 import uuid
+import sys
+from os.path import dirname, abspath, join as joinpath
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 import copy
 import urllib.parse
 from pathlib import Path
