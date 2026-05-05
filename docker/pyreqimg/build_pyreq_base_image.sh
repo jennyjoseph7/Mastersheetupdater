6aa1513d845352bf6f7042e669402e1357c6c4b5
sreenath <sreenath@iamdave.ai>
2026-05-05 15:36:27 +0530

    added scripts to build docker image in layers

---
+++ docker/pyreqimg/build_pyreq_base_image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function update_repo() {
+	sha=$1
+	git reset --hard HEAD^
+	git pull origin $1
+	git checkout $1
+
+}
+
+function main() {
+	update_repo "master"
+	cp ../../requirements.txt ./
+    cp ../../spark/requirements.txt ./spark_requirements.txt
+	docker build -t autobot_pyreq_baseimage:latest .	
+	docker tag autobot_pyreq_baseimage:latest asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-base-image/pyreq_base_image:latest
+	docker push asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-base-image/pyreq_base_image:latest
+}
+
+main
