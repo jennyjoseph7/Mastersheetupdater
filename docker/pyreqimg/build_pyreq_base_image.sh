8490db65c5873e448cffc2866fef2ebcda6a5522
sreenath <sreenath@iamdave.ai>
2026-05-05 16:14:14 +0530

    updated baseimage

--- docker/pyreqimg/build_pyreq_base_image.sh
+++ docker/pyreqimg/build_pyreq_base_image.sh
@@ -13,8 +13,8 @@ function main() {
 	cp ../../requirements.txt ./
     cp ../../spark/requirements.txt ./spark_requirements.txt
 	docker build -t autobot_pyreq_baseimage:latest .	
-	docker tag autobot_pyreq_baseimage:latest asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-base-image/pyreq_base_image:latest
-	docker push asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-base-image/pyreq_base_image:latest
+	docker tag autobot_pyreq_baseimage:latest asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-baseimage/autobot-pyreq-baseimage:latest
+	docker push asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-baseimage/autobot-pyreq-baseimage:latest
 }
 
 main
