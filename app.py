ce2f15b4ee93e32a9e710c6acce6a1e935fc51c9
aryan_mac <aryan@iamdave.ai>
2026-05-13 18:03:31 +0530

    Add cron job for daily dealership summary task

--- app.py
+++ app.py
@@ -96,6 +96,14 @@ def SETUP(skip_models = False, skip_data = False, start_models_from = None, star
               add_schedule_to_queue=False
         )
         
+        cron_worker.add_cron_job(
+            enterprise_id=AUTOCRM_APP_ENTERPRISE_ID,
+              task="daily_dealership_summary",
+              service=AUTOCRM_CRON_SERVICE_NAME,
+             schedule = "*/10 * * * *",
+              add_schedule_to_queue=False
+        )
+        
         # cron_worker.add_cron_job(
         #     enterprise_id=AUTOCRM_APP_ENTERPRISE_ID,
         #       task="set_worker_count",
