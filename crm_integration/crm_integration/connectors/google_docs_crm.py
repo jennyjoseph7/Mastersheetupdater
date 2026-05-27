867314ad54b53a200601ce2cf5155e4c0412233a
Sahib <sahib@iamdave.ai>
2026-05-28 00:19:44 +0530

    fixed issues in google docs crm, cron.py, campaign_workflow

--- crm_integration/crm_integration/connectors/google_docs_crm.py
+++ crm_integration/crm_integration/connectors/google_docs_crm.py
@@ -2,7 +2,6 @@ try:
     import gspread
 except ImportError:
     gspread = None
-
 from google.oauth2.service_account import Credentials
 from crm_integration.crm_integration.base_crm import BaseCRMClass
 
