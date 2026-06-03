12ec07bbdde9dbfc77804d52dac1062fe9d60773
dave_gateway <dave@iamdave.ai>
2026-06-03 11:42:55 +0000

    remove old Dockerfile (unified build version)

---
+++ docker/pyreqimg/autobot_agents/build_pyreq_base_image.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export pull_success=0
+
+function update_repo() {
+	sha=$1
+	git reset --hard HEAD^
+	git checkout $1
+	git pull origin $1
+	status=$?
+	pull_success=1
+	if [ $status != 0 ];then
+		echo "Pull branch $1 failed."
+		pull_success=0
+	fi
+}
+
+function main() {
+	update_repo $1
+	if [ $pull_success == 0 ];then
+		return
+	fi
+	cp ../../../requirements.txt ./
+   # cp ../../spark/requirements.txt ./spark_requirements.txt
+	
+	sed -i "s/<baseimage_tag>/$TAG/g" Dockerfile
+
+	docker build -t autobot_pyreq_baseimage:$TAG .	
+	docker tag autobot_pyreq_baseimage:$TAG asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-baseimage/autobot-pyreq-baseimage:$TAG
+	docker push asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-baseimage/autobot-pyreq-baseimage:$TAG
+}
+
+export BRANCH=${1:-"master"}
+
+if [ "$BRANCH" == "master" ];then
+	export TAG="master"
+else
+	export TAG=$BRANCH
+fi
+
+main 
