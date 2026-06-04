a9f46c47ed9d0159c3934e9201cee9bc84c683f2
dave_gateway <dave@iamdave.ai>
2026-06-04 07:57:22 +0000

    removed uneccessary dir

--- docker/pyreqimg/build_pyreq_base_image.sh
+++ docker/pyreqimg/build_pyreq_base_image.sh
@@ -1,101 +1,87 @@
 #!/bin/bash
 
+set -euo pipefail
+
 BRANCH=${1:-master}
-SERVICE=${2:-document_processor}
-
-DOCKERFILE=${DOCKERFILE:-Dockerfile}
-pull_success=0
-
-declare -A REPOS
-REPOS["document_processor"]="asia-south1-docker.pkg.dev/dave-70c8e/document-processor-pyreq-image/autobot-pyreq-baseimage"
-REPOS["spark"]="asia-south1-docker.pkg.dev/dave-70c8e/spark-pyreq-image/autobot-pyreq-baseimage"
-REPOS["autobot_agents"]="asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-baseimage/autobot-pyreq-baseimage"
+SERVICE=${2:-autobot_agents}
 
 update_repo() {
+    git fetch origin "$BRANCH"
+    git checkout "$BRANCH" || git checkout -b "$BRANCH" "origin/$BRANCH"
+    git reset --hard "origin/$BRANCH"
+}
 
-    branch=$1
+prepare_service() {
 
-    git fetch origin "$branch"
-    git checkout "$branch" || git checkout -b "$branch" origin/"$branch"
-    git reset --hard origin/"$branch"
+    if [ "$SERVICE" = "autobot_agents" ]; then
 
-    if [ $? != 0 ]; then
-        echo "Pull branch $branch failed."
-        pull_success=0
-        return
+        cp ../../requirements.txt .
+
+        REPO="asia-south1-docker.pkg.dev/dave-70c8e/autobot-pyreq-baseimage/autobot-pyreq-baseimage"
+
+        REQS='COPY requirements.txt /tmp/
+RUN /root/pyenv/bin/pip install --ignore-installed -r /tmp/requirements.txt'
+
+    elif [ "$SERVICE" = "spark" ]; then
+
+        cp ../../spark/requirements.txt spark_requirements.txt
+
+        REPO="asia-south1-docker.pkg.dev/dave-70c8e/spark-pyreq-image/autobot-pyreq-baseimage"
+
+        REQS='COPY spark_requirements.txt /tmp/
+RUN /root/pyenv/bin/pip install --ignore-installed -r /tmp/spark_requirements.txt'
+
+    elif [ "$SERVICE" = "document_processor" ]; then
+
+        cp ../../document_processor/requirment_document_processor.txt document_processor_requirements.txt
+        cp ../../brochure_pipeline/requirements.txt brochure_pipeline_requirements.txt
+
+        REPO="asia-south1-docker.pkg.dev/dave-70c8e/document-processor-pyreq-image/autobot-pyreq-baseimage"
+
+        REQS='COPY document_processor_requirements.txt /tmp/
+COPY brochure_pipeline_requirements.txt /tmp/
+RUN /root/pyenv/bin/pip install --ignore-installed -r /tmp/document_processor_requirements.txt
+RUN /root/pyenv/bin/pip install --ignore-installed -r /tmp/brochure_pipeline_requirements.txt'
+
+    else
+        echo "Invalid service: $SERVICE"
+        exit 1
     fi
+}
 
-    pull_success=1
+generate_dockerfile() {
+
+    cat > Dockerfile.tmp <<EOF
+FROM asia-south1-docker.pkg.dev/dave-70c8e/autobot-base-image/base_image:${BRANCH}
+
+WORKDIR /root/
+
+RUN update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+RUN /root/pyenv/bin/pip install -U pip setuptools
+
+${REQS}
+EOF
 }
 
 build_image() {
 
-    DOCKERFILE_LOCAL=$1
-
-    echo "Using Dockerfile: $DOCKERFILE_LOCAL"
-
     docker build \
-        -t autobot-pyreq-baseimage:$SERVICE-$BRANCH \
-        -f "$DOCKERFILE_LOCAL" \
+        -f Dockerfile.tmp \
+        -t autobot-pyreq-baseimage:${SERVICE}-${BRANCH} \
         .
 
     docker tag \
-        autobot-pyreq-baseimage:$SERVICE-$BRANCH \
-        ${REPOS[$SERVICE]}:$BRANCH
+        autobot-pyreq-baseimage:${SERVICE}-${BRANCH} \
+        ${REPO}:${BRANCH}
 
-    docker push ${REPOS[$SERVICE]}:$BRANCH
+    docker push ${REPO}:${BRANCH}
 }
 
 main() {
-
-    if [ -z "${REPOS[$SERVICE]}" ]; then
-        echo "Invalid service: $SERVICE"
-        exit 1
-    fi
-
-    update_repo "$BRANCH"
-
-    if [ $pull_success == 0 ]; then
-        exit 1
-    fi
-
-    echo "Branch: $BRANCH"
-    echo "Service: $SERVICE"
-
-    if [ "$SERVICE" == "document_processor" ]; then
-
-        cp ../../brochure_pipeline/requirements.txt ./brochure_pipeline_requirements.txt
-        cp ../../document_processor/requirment_document_processor.txt ./document_processor_requirements.txt
-
-        cp Document_BrochureDockerfile Document_BrochureDockerfile.tmp
-        sed -i "s/<baseimage_tag>/$BRANCH/g" Document_BrochureDockerfile.tmp
-
-        build_image Document_BrochureDockerfile.tmp
-
-        rm -f Document_BrochureDockerfile.tmp
-
-    elif [ "$SERVICE" == "spark" ]; then
-
-        cp ../../spark/requirements.txt ./spark_requirements.txt
-
-        cp SparkDockerfile SparkDockerfile.tmp
-        sed -i "s/<baseimage_tag>/$BRANCH/g" SparkDockerfile.tmp
-
-        build_image SparkDockerfile.tmp
-
-        rm -f SparkDockerfile.tmp
-
-    elif [ "$SERVICE" == "autobot_agents" ]; then
-
-        cp ../../requirements.txt ./
-
-        cp AutobotDockerfile AutobotDockerfile.tmp
-        sed -i "s/<baseimage_tag>/$BRANCH/g" AutobotDockerfile.tmp
-
-        build_image AutobotDockerfile.tmp
-
-        rm -f AutobotDockerfile.tmp
-    fi
+    update_repo
+    prepare_service
+    generate_dockerfile
+    build_image
 }
 
 main
