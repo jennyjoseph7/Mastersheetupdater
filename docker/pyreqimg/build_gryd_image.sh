24437ed3166319cee74989ab731711998c52937a
dave_gateway <dave@iamdave.ai>
2026-06-08 10:01:15 +0000

    updated build script for monitoring purpose

--- docker/pyreqimg/build_gryd_image.sh
+++ docker/pyreqimg/build_gryd_image.sh
@@ -11,18 +11,17 @@ CONFIG_FILE="${CONF_DIR}/${SERVICE}.conf"
 cd "$BASE_DIR"
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    echo "Config not found: $CONFIG_FILE"
     exit 1
 fi
 
 source "$CONFIG_FILE"
 
 if [ -z "${REQUIREMENTS_SOURCE:-}" ]; then
-    echo "REQUIREMENTS_SOURCE is not set"
     exit 1
 fi
 
 update_repo() {
+    git reset --hard
     git fetch origin "$BRANCH"
     git checkout "$BRANCH" || git checkout -b "$BRANCH" "origin/$BRANCH"
     git pull origin "$BRANCH"
@@ -37,19 +36,15 @@ generate_dockerfile_and_context() {
     for item in "${items[@]}"; do
         item=$(echo "$item" | xargs)
 
-        # HOST PATH (works on your machine)
         if [ ! -f "$item" ]; then
-            echo "Missing file on host: $item"
             exit 1
         fi
 
         filename=$(basename "$item")
 
-        # COPY INTO BUILD CONTEXT
         cp "$item" "$BASE_DIR/$filename"
 
-        # USE RELATIVE PATH INSIDE DOCKER
-	echo "RUN /root/pyenv/bin/pip install --upgrade --force-reinstall --no-cache-dir -r $filename" >> Dockerfile.tmp
+        echo "RUN /root/pyenv/bin/pip install --upgrade --force-reinstall --no-cache-dir -r $filename" >> Dockerfile.tmp
     done
 }
 
@@ -66,22 +61,6 @@ build_image() {
     docker push ${REPO}:${BRANCH}
 }
 
-cleanup() {
-    rm -f Dockerfile.tmp
-
-    # remove copied requirement files only
-    IFS=, read -ra items <<< "$REQUIREMENTS_SOURCE"
-    for item in "${items[@]}"; do
-        filename=$(basename "$item")
-        rm -f "$BASE_DIR/$filename"
-    done
-}
-
-main() {
-    update_repo
-    generate_dockerfile_and_context
-    build_image
-    cleanup
-}
-
-main
+update_repo
+generate_dockerfile_and_context
+build_image
