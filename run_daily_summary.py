25b24f335eba5a683513494b1bbaf9f3f6ea15fb
aryan_mac <aryan@iamdave.ai>
2026-06-15 19:46:49 +0530

    Add loading of stored procedures in daily summary run

--- run_daily_summary.py
+++ run_daily_summary.py
@@ -1,6 +1,9 @@
 from autocrm_db_helper import get_pg_connector
+from analytics.loader import load_stored_procedures
 
 def run_summary():
+    print("Loading stored procedures...")
+    load_stored_procedures()
     print("Running update_daily_dealership_summary...")
     with get_pg_connector() as pg:
         pg.execute_write("CALL update_daily_dealership_summary();", _fetch=False)
